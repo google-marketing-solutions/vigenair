645185e02e30837e3c71f71327aca9f1c99ca302
Mohab Fekry <mfekry@google.com>
2024-04-06 12:50:50 +0200

    Switch to MIT license
    Change-Id: I074933aabf411be52a4e413cee5a0a10f75cf67c

--- service/deploy.sh
+++ service/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2024 Google LLC.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the MIT License;
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
